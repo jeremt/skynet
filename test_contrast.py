b2c36ff687fbb851803e7381075e2d4ec53154ed
Jeremie Taboada <taboada.jeremie@gmail.com>
2014-10-07 21:25:28 +0100

    Add color teacher

--- test_contrast.py
+++ test_contrast.py
@@ -11,6 +11,33 @@ def rand_bw():
 def rand_color():
     return randrange(0, 256), randrange(0, 256), randrange(0, 256)
 
+def normalize(color):
+    return map(lambda x: x / 255., color)
+
+def brightness(color):
+    return 0.299 * color[0] + 0.587 * color[1] + 0.114 * color[2]
+
+class ColorTeacher(object):
+
+    def __init__(self, network):
+        self.network = network
+
+    def train(self, n=1000, verbose=False):
+        """ Train the neural network by feeding the network and apply the back
+            propagation. The learning rate is represented by `eta` while
+            `alpha` is the momentum factor.
+        """
+        if verbose:
+            print("\nTrain the network over %d iterations:\n" % n)
+        for i in range(n):
+            color = rand_color()
+            self.network.feed_forward(color)
+            error = self.network.back_propagate(
+                [1. - brightness(normalize(color))]
+                )
+            if verbose and i % (n / 10) == 0:
+                print("  Error: %.8f" % error)
+
 class Window(QtGui.QWidget):
 
     def __init__(self):
@@ -21,13 +48,16 @@ class Window(QtGui.QWidget):
             eta=0.2, alpha=0.02,
             )
 
+        teacher = ColorTeacher(self.net)
+        teacher.train(100000, verbose=True)
+
         layout = QtGui.QVBoxLayout(self)
 
         buttons_layout = QtGui.QHBoxLayout()
         layout.addLayout(buttons_layout)
 
         color = rand_color()
-        self.inputs = map(lambda x: x / 255., color)
+        self.inputs = normalize(color)
         self.black_button = QtGui.QPushButton('BLACK', self)
         self.black_button.clicked.connect(self.on_black)
         self.update_button_style('black', color)
@@ -61,22 +91,23 @@ class Window(QtGui.QWidget):
 
     def on_black(self):
         color = rand_color()
-        self.inputs = map(lambda x: x / 255., color)
+        self.inputs = normalize(color)
         self.update_button_style('black', color)
         self.update_button_style('white', color)
         self.train(self.inputs, [1.0])
         
     def on_white(self):
         color = rand_color()
-        self.inputs = map(lambda x: x / 255., color)
+        self.inputs = normalize(color)
         self.update_button_style('black', color)
         self.update_button_style('white', color)
         self.train(self.inputs, [0.0])
 
     def on_auto(self):
         result = self.net.feed_forward(self.inputs)[0]
-        self.label.setText("Result: %s, Choice: %s" % (
-            result, ("white" if result < 0.5 else "black")
+        self.label.setText("Result: %.2f, Choice: %s, Brighness: %.2f" % (
+            result, ("white" if result < 0.5 else "black"),
+            brightness(self.inputs),
             ))
 
 if __name__ == '__main__':
